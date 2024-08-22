74670e97f98e39aa6e16d54792807ba4f8fe92cd
Rob <61951374+Leo4j@users.noreply.github.com>
2024-08-22 14:11:29 +0100

    Update PS2EXE.ps1

--- PS2EXE.ps1
+++ PS2EXE.ps1
@@ -1,7 +1,7 @@
 function PS2EXE {
     Param (
         [string]$content,
-		[string]$outputFile
+	[string]$outputFile
     )
 
     $script = [System.Convert]::ToBase64String(([System.Text.Encoding]::UTF8.GetBytes($content)))
@@ -11,6 +11,7 @@ using System;
 using System.Management.Automation;
 using System.Text;
 using System.Reflection;
+using System.Collections.ObjectModel;
 
 namespace ModuleNamespace
 {
@@ -21,7 +22,24 @@ namespace ModuleNamespace
             string script = @"$script";
             PowerShell ps = PowerShell.Create();
             ps.AddScript(Encoding.UTF8.GetString(Convert.FromBase64String(script)));
-            ps.Invoke();
+
+            // Execute the script and capture the output
+            Collection<PSObject> results = ps.Invoke();
+
+            // Print each result to the console
+            foreach (var result in results)
+            {
+                Console.WriteLine(result.ToString());
+            }
+
+            // If there are errors, print them as well
+            if (ps.HadErrors)
+            {
+                foreach (var error in ps.Streams.Error)
+                {
+                    Console.WriteLine("Error: " + error.ToString());
+                }
+            }
         }
     }
 }
